e3aacbbf78e7f55c2ccd50855e2bb9e6601c060c
JoseEPina <jose.edpina@gmail.com>
2021-08-15 14:40:56 -0600

    Refactorization: createTaskElement as its own func. taskFormHandler now only receives input.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,22 @@
 var formElement = document.querySelector("#task-form");
 var tasksToDoElement = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function (event) {
+var taskFormHandler = function (event) {
    event.preventDefault();
-
    var taskNameInput = document.querySelector("input[name='task-name']").value;
    var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+   // Package up date as an object
+   var taskDataObj = {
+      name: taskNameInput,
+      type: taskTypeInput,
+   };
+
+   // Send it as an argument to createTaskElement
+   createTaskElement(taskDataObj);
+};
+
+var createTaskElement = function (taskDataObj) {
    // Create list item
    var listItemElement = document.createElement("li");
    // Give it a class name
@@ -16,16 +26,14 @@ var createTaskHandler = function (event) {
    var taskInfoElement = document.createElement("div");
    // Give it a class name
    taskInfoElement.className = "task-info";
-
    // Add HTML content to div item
    taskInfoElement.innerHTML =
-      "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+      "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
 
    listItemElement.appendChild(taskInfoElement);
 
    // Add entire list item to list
    tasksToDoElement.appendChild(listItemElement);
-   console.dir(listItemElement);
 };
 
-formElement.addEventListener("submit", createTaskHandler);
+formElement.addEventListener("submit", taskFormHandler);
